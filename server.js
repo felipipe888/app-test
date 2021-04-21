5f8192ad2cbabf42d780f2996bce84f66783dc45
Felipe Chacon <afelipe.chacon@gradiweb.com>
2021-04-21 09:00:30 -0500

    structure project

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const fetch = require("node-fetch");
+require('dotenv')
+
+const app = express();
+
+const port = process.env.PORT || 3000
+
+app.get("/customer", (req, res) => {
+  fetch("https://andres-test-gradiweb.myshopify.com/admin/api/2021-04/customers.json", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "X-Shopify-Access-Token": "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",
+    }  
+    })
+    .then((result) => {
+      return result.json();
+    })
+    .then((data) => {
+    //   console.log("data returned:\n", data);
+      let {customers} = data;
+
+      let exist = customers.find((user)=>user.email === req.query.email)
+
+      if(exist){
+          res.send({data:exist, status: false});
+      }else{
+          res.send({status:true});
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+app.listen(port, () => console.log("Listening on port: " + port));
