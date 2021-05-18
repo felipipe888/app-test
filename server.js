75fa045cf8c73a3ac0ea252a0eb6f6890014d32e
Felipe Chacon <felipipe88806@gmail.com>
2021-05-18 17:55:14 -0500

    [UPDATE]

--- server.js
+++ server.js
@@ -156,7 +156,7 @@ app.get("/products", (req, res) => {
 });
 
 app.put("/product/update", (req, res) => {
-  console.log(req.body);
+  console.log(req.body.formdata);
   let products = JSON.parse(req.body)
   console.log(products);
   fetch(`https://mykabuto.myshopify.com/cart/update.js`, {
